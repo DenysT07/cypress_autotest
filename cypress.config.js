abd70c0451826f6acbcbffa8ae3223b4097618ea
DenysT0707 <denist0707@gmail.com.com>
2022-09-25 16:04:31 +0300

    update5

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   viewportWidth: 1080,
   viewportHeight: 1920,
+  projectId: "459tn7",
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
